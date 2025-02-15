5073219c876aa13782d14952e7cc5cc76256482b
santatra <mariosantatra@gmail.com>
2025-02-15 09:01:21 +0300

    add hover to social icon and remove error fuctionality to the form

--- src/components/Form/FormContact.tsx
+++ src/components/Form/FormContact.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Input from '../common/Input/Input'
 import TextArea from '../common/TextArea/TextArea'
 import Button from '../common/Button/Button'
@@ -17,9 +17,7 @@ function FormContact() {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [message, setMessage] = useState('')
-  const [errorClass, setErrorClass] = useState<InputErroClassNmaes>({
-    name: false, email: false, message: false
-  })
+
   const sendMail = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     setLoad(true)
@@ -29,11 +27,7 @@ function FormContact() {
       message: message
     }
 
-    if (message === "") setErrorClass({ ...errorClass, message: true })
-    if (email === "") setErrorClass({ ...errorClass, email: true })
-    if (name === "") setErrorClass({ ...errorClass, name: true })
-
-    if (name.trim() === "" || email.trim() === "" || message.trim() === "") {
+    if (!name.trim() || !email.trim() || !message.trim()) {
       Swal.fire({
         icon: "error",
         title: "Oops...",
@@ -43,7 +37,6 @@ function FormContact() {
       setLoad(false)
       return
     } else {
-      setErrorClass({ name: false, email: false, message: false })
       emailjs
         .send("service_56bza87", "template_8phdps8", templateParams, "Y55U-eVuDLLpHV7wh")
         .then(
@@ -83,7 +76,6 @@ function FormContact() {
       <Input
         name='name'
         placeholder='NAME'
-        clasName={errorClass.name ? "error" : ""}
         value={name}
         onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
       />
@@ -91,14 +83,12 @@ function FormContact() {
         name='email'
         type='email'
         placeholder='E-MAIL'
-        clasName={errorClass.email ? "error" : ""}
         value={email}
         onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
       />
       <TextArea
         name='message'
         placeholder='MESSAGE'
-        clasName={errorClass.message ? "error" : ""}
         value={message}
         onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setMessage(e.target.value)}
       />
